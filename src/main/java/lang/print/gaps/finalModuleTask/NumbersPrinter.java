8b2d243a634932058c3a38f96496ae5fc3d47111
schedulerz <thisisabilkair.zhakupov@gmail.com>
2022-08-02 20:40:38 +0600

    Number #1

--- src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
@@ -3,5 +3,12 @@ package lang.print.gaps.finalModuleTask;
 public class NumbersPrinter {
     public void printIsPositive(int number) {
 
+        System.out.println(text[index(number)]);
     }
+
+    int index(int i) {
+        return 1 + (i>>31) - (-i>>31);
+    }
+
+    String[] text = {"false", "false", "true"};
 }
